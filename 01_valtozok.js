2dafb395f8d22221fb900e3103154528a8c8a91c
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2025-02-04 09:09:50 +0100

    a

--- 01_valtozok.js
+++ 01_valtozok.js
@@ -1,10 +1,18 @@
-//
+// Változó deklarálása használat előtt megjelenítés: ELVAULT
 var a = 0;
 console.log(a);
-//
+//Váltoó értékének módosítása
 let b = 3;
 b = 4;
 console.log(b);
 const c = 5;
 console.log(c);
-//c = 6; 
+//c = 6; //hiba
+
+//Var váltzozó újradefiniálása
+var x = 5;
+console.log(x);
+var x = "alma";
+console.log(x);
+
+let d = 3;
